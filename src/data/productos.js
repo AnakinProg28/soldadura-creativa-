dd3b5ec1ed5feab0aec5ca2ea7f297f21686db26
AnakinProg28 <anakinreateguirios@gmail.com>
2026-09-16 21:56:52 -0500

    Update image URL for 'Enamorados' product

--- src/data/productos.js
+++ src/data/productos.js
@@ -11,7 +11,7 @@ export const CATEGORIAS = [
     icono: '❤️',
     subtitulo: 'Regalos y detalles eternos forjados en metal',
     descripcion: 'Piezas artesanales creadas para simbolizar un amor inquebrantable. Forjadas a fuego lento con detalles florales, corazones entrelazados y acabados brillantes protegidos contra la corrosión.',
-    imagen: 'https://i.ibb.co/zW3gYB5J/ROSAS-ROJAS.jpg'
+    imagen: 'https://i.ibb.co/XxPBYy0J/PORTADA.jpg'
   },
   {
     id: 'cumpleanos',
